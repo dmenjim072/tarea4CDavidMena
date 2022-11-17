a65d42f2522441cd7ad14f42a993abd75a93b5b4
david <dmenjim072@g.educaand.es>
2022-11-17 12:44:17 +0100

    terminar

--- src/main/java/clases/Persona.java
+++ src/main/java/clases/Persona.java
@@ -4,6 +4,8 @@
  */
 package clases;
 
+import static java.lang.Math.random;
+import java.util.Random;
 
 /**
  *
@@ -15,44 +17,137 @@ public class Persona {
     private int edad;
     private String NIF;
     private char sexo;
-    private double peso;
-    private int altura;
+    private double peso; //KG
+    private int altura; //cm
     
     public Persona() {
     
     }
 
     
-    public Persona(String nombre, int edad, String NIF, char sexo, double peso, int altura) {
+    public Persona(String nombre, int edad, char sexo, double peso, int altura) {
         this.nombre = nombre;
-        this.edad = edad;
-        this.NIF = NIF;
-        this.sexo = sexo;
+        this.edad = edad;        
+        this.sexo='O';
+        if (filtrarSexo(sexo)){
+            this.sexo = sexo;
+        }
         this.peso = peso;
         this.altura = altura;
+        this.NIF = generaNIF();
     }
     
-    public boolean filtrarSexo(char sexo){
+    private boolean filtrarSexo(char sexo){
         
-        boolean sexoFiltrado = true;
+        return (sexo == 'H' || sexo == 'M' || sexo == 'O');
+    }
+    
+    private static String generaNIF(){
+               
+        String nif = "";        
+        int letraNIF = 0;    
+        String letra = "";
         
-        if (sexo != 'H' || sexo != 'M' || sexo != 'O'){
-            sexoFiltrado = false;
+        Random numAleatorio = new Random();
+        int numeroRandom = numAleatorio.nextInt(1, 99999999);
+                
+        letraNIF = numeroRandom % 23;
+        
+        switch (letraNIF){
+            case 0:
+                letra = "T";
+                break;
+            case 1:
+                letra = "R";
+                break;
+            case 2:
+                letra = "W";
+                break;
+            case 3:
+                letra = "A";
+                break;    
+            case 4:
+                letra = "G";
+                break;
+            case 5:
+                letra = "M";
+                break;
+            case 6:
+                letra = "Y";
+                break;
+            case 7:
+                letra = "F";
+                break;
+            case 8:
+                letra = "P";
+                break;
+            case 9:
+                letra = "D";
+                break;
+            case 10:
+                letra = "X";
+                break;
+            case 11:
+                letra = "B";
+                break;
+            case 12:
+                letra = "N";
+                break;
+            case 13:
+                letra = "J";
+                break;
+            case 14:
+                letra = "Z";
+                break;
+            case 15:
+                letra = "S";
+                break;
+            case 16:
+                letra = "Q";
+                break;
+            case 17:
+                letra = "V";
+                break;
+            case 18:
+                letra = "H";
+                break;
+            case 19:
+                letra = "L";
+                break;
+            case 20:
+                letra = "C";
+                break;
+            case 21:
+                letra = "K";
+                break;
+            case 22:
+                letra = "E";
+                break;            
         }
+               
+        numeroRandom = Integer.valueOf(numeroRandom);
         
-        return sexoFiltrado;
+        nif = numeroRandom + letra;
+        
+        return nif;
+        }
+    
+    public boolean esMayorEdad(){
+        return this.edad>=18;
     }
     
-    public String generaNIF(){
-        
-        int numeroNIF = 0;
-        int numeroAleatorio = 0;
-        String letraNIF = "";    
-                     
-        numeroNIF = numeroAleatorio.random
-        
-        letraNIF = numeroNIF % 23;
-    
-    return ;
+    public float calcularIMC(){
+        return (float)(peso/Math.pow(this.altura, 2));
     }
+    
+    public void darLike(Serie serie){
+        serie.darLike();
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", edad=" + edad + ", peso=" + peso + ", altura=" + altura + '}';
+    }
+    
+    
 }
---
+++ src/main/java/clases/pruebas.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author david
+ */
+public class pruebas {
+    
+    public static void main(String[] args) {
+        
+        Persona juan = new Persona();
+        System.out.println(juan);
+        Persona maria = new Persona("Maria", 23, 60.3, 1, );
+        
+        Serie s1 = new Serie();
+        System.out.println(s1);
+        maria.darLike(s1);
+        System.out.println(s1);
+        
+        System.out.println(maria);
+        juan.setEdad(15);
+        System.out.println("IMC maria");
+    }
+}
